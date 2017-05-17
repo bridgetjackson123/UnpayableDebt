894bfe08dc43fd9e4f4703fed41fd3b57017a76e
bridgetjackson123 <bmj2125@barnard.edu>
2017-05-17 16:24:31 -0700

    Delete dtTest.py

--- dtTest.py
+++
@@ -1,7 +0,0 @@
-
-wordString = 'Hello" Im" brdig.et.scnin, o	23b2.3?'
-#wordString = wordString.translate(None, '-\'!@#$%"",.?!')
-lenWord = len(wordString)
-if(wordString[lenWord-1] == '?'):
-	print "hi"
-print wordString
